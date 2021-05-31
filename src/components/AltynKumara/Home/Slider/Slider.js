cf592f0ef275a910c4f1f64a0e3b1f54b0ffa023
Baitemir <ferrari170604@gmail.com>
2021-05-31 22:23:39 +0600

    finally fixed slider

--- src/components/AltynKumara/Home/Slider/Slider.js
+++ src/components/AltynKumara/Home/Slider/Slider.js
@@ -5,25 +5,45 @@ import image2 from "../../../../images/slider-images/image2.png";
 import image3 from "../../../../images/slider-images/image3.jpg";
 
 const Slider = () => {
-  const slideColors = [
-    image1,
-    image2,
-    image3
-  ]
-  const [currentSlide, setCurrentSlide] = useState(0)
 
-const slides = slideColors.length;
-useEffect(()=>{
-  setInterval(()=>{
-    setCurrentSlide(
-      currentSlide === slides - 1 ? 0 : currentSlide + 1
-    )
-  }, 10000)
-}, [currentSlide, slides])
+
+  const [currentSlide, setCurrentSlide] = useState(1);
+
+  let move = '0px'
+  
+  switch (currentSlide) {
+    case 1:
+      move = '0px'
+      break;
+    case 2:
+      move = '-100%'
+      break;
+    case 3:
+      move = '-200%'
+      break;
+    default:
+      move = '0px'
+      break;
+  }
+  if(currentSlide === 4){
+    setCurrentSlide(1)
+  }
+  if(currentSlide < 1){
+    setCurrentSlide(3)
+  }
+
+
+
 
   return ( <div className={classes.Slider}>
-    <div style={{backgroundImage: `url(${slideColors[currentSlide]})`}} className={classes.slide}></div>
-    <div>
+    <div style={{transform: `translateX(${move})`}} className={classes.slides}>
+      <div style={{backgroundImage: `url(${image1})`}}></div>
+      <div style={{backgroundImage: `url(${image2})`}}></div>
+      <div style={{backgroundImage: `url(${image3})`}}></div>
+    </div>
+    <div className={classes.sliderBtns}>
+      <button onClick={()=> setCurrentSlide(currentSlide - 1)} className={classes.sliderBtn}>{'<'}</button>
+      <button onClick={()=> setCurrentSlide(currentSlide + 1)} className={classes.sliderBtn}>{'>'}</button>
     </div>
   </div> );
 }
