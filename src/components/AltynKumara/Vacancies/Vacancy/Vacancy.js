f4bf7383527c542d4f9078de70bac239492c47fc
Baitemir <ferrari170604@gmail.com>
2021-05-31 13:04:32 +0600

    setted select inputs

--- src/components/AltynKumara/Vacancies/Vacancy/Vacancy.js
+++ src/components/AltynKumara/Vacancies/Vacancy/Vacancy.js
@@ -3,6 +3,48 @@ import { useHistory } from "react-router";
 import classes from "./Vacancy.module.css";
 
 const Vacancy = ({ match }) => {
+  const occupations = [];
+
+
+  switch(match.params.id){
+    case 'waiter':
+      occupations.push(
+        <select required name='occupation'>
+          <option>Официант</option>
+          <option>Раннер</option>
+          <option>Хостес</option>
+        </select>
+      )
+      break
+    case 'admin':
+      occupations.push(
+        <select required name='occupation'>
+          <option>Администратор</option>
+          <option>Кассир</option>
+        </select>
+      )
+      break
+    case 'chef':
+      occupations.push(
+        <select required name='occupation'>
+          <option>Повар универсал</option>
+          <option>Шашлычник</option>
+          <option>Салатник</option>
+          <option>Суши-мастер</option>
+          <option>Пиццейола</option>
+        </select>
+      )
+      break
+    case 'cleaner':
+      occupations.push(
+        <select required name='occupation'>
+          <option>Посудомойщица</option>
+          <option>Уборщица</option>
+        </select>
+      )
+      break
+  
+  }
 
   const history = useHistory();
 
@@ -36,22 +78,19 @@ const Vacancy = ({ match }) => {
         <label htmlFor='age'>Возраст</label>
         <input required type='number' name='age' />
         <label htmlFor='occupation'>Должность</label>
-        <select required name='occupation'>
-          <option>/-/-/-/-/-/-/-/</option>
-          <option>-</option>
-          <option>-</option>
-        </select>
+        {occupations}
         <label htmlFor='exp'>Стаж работы</label>
         <select required name='exp'>
-          <option>/-/-/-/-/-/-/-/</option>
-          <option>-</option>
-          <option>-</option>
+          <option>Без опыта</option>
+          <option>Меньше года</option>
+          <option>1-4 года</option>
+          <option>Больше 4 лет</option>
         </select>
         <label htmlFor='graph'>График</label>
         <select required name='graph'>
-          <option>/-/-/-/-/-/-/-/</option>
-          <option>-</option>
-          <option>-</option>
+          <option>3 дня в неделю</option>
+          <option>4 дня в неделю</option>
+          <option>5 дней в неделю</option>
         </select>
         <label htmlFor='phone'>Номер телефона</label>
         <input required type='phone' name='phone' />
