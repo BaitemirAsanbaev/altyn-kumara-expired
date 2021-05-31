bd3acedd7ec6e8d1f420f43164c3fe84995424e9
Baitemir <ferrari170604@gmail.com>
2021-05-31 22:54:43 +0600

    added responsibility to home page

--- src/components/AltynKumara/Home/Home.js
+++ src/components/AltynKumara/Home/Home.js
@@ -6,17 +6,19 @@ import kumara_logo from "../../../images/kumara.png"
 const Home = () => {
   return ( <div className={classes.Home}>
     <Slider/>
-    <Best/>
-    <div className={classes.info}>
-      <div>
-        <h2>Алтын</h2>
-        <img alt='altyn' src={kumara_logo}/>
-        <h2>Кумара</h2>
-      </div>
-      <div>
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad earum eaque autem impedit laborum optio nesciunt omnis quibusdam consectetur nisi sequi suscipit maiores, numquam reprehenderit, eos itaque. Ex, vel voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad earum eaque autem impedit laborum optio nesciunt omnis quibusdam consectetur nisi sequi suscipit maiores, numquam reprehenderit, eos itaque. Ex, vel voluptatem. 
-        </p>
+    <div className={classes.home}>
+      <Best/>
+      <div className={classes.info}>
+        <div>
+          <h2>Алтын</h2>
+          <img alt='altyn' src={kumara_logo}/>
+          <h2>Кумара</h2>
+        </div>
+        <div>
+          <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad earum eaque autem impedit laborum optio nesciunt omnis quibusdam consectetur nisi sequi suscipit maiores, numquam reprehenderit, eos itaque. Ex, vel voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad earum eaque autem impedit laborum optio nesciunt omnis quibusdam consectetur nisi sequi suscipit maiores, numquam reprehenderit, eos itaque. Ex, vel voluptatem. 
+          </p>
+        </div>
       </div>
     </div>
   </div> );
