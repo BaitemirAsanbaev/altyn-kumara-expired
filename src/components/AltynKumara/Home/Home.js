55a378a6160329b25dd0f84f12916c0ef599a5e2
Baitemir <ferrari170604@gmail.com>
2021-05-31 13:52:02 +0600

    added info block in home page

--- src/components/AltynKumara/Home/Home.js
+++ src/components/AltynKumara/Home/Home.js
@@ -1,11 +1,24 @@
 import classes from "./Home.module.css";
 import Slider from "./Slider/Slider"
 import Best from "./Best/Best"
+import kumara_logo from "../../../images/kumara.png"
 
 const Home = () => {
   return ( <div className={classes.Home}>
     <Slider/>
     <Best/>
+    <div className={classes.info}>
+      <div>
+        <h2>Алтын</h2>
+        <img alt='altyn' src={kumara_logo}/>
+        <h2>Кумара</h2>
+      </div>
+      <div>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad earum eaque autem impedit laborum optio nesciunt omnis quibusdam consectetur nisi sequi suscipit maiores, numquam reprehenderit, eos itaque. Ex, vel voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad earum eaque autem impedit laborum optio nesciunt omnis quibusdam consectetur nisi sequi suscipit maiores, numquam reprehenderit, eos itaque. Ex, vel voluptatem. 
+        </p>
+      </div>
+    </div>
   </div> );
 }
  
