30bcdc6c5de78be1ed07afa8a822d89c80906faa
Baitemir Asanbaev <baitemir1706@gmail.com>
2021-08-14 15:24:51 +0600

    addded alt prop

--- src/components/UI/UpBtn/UpBtn.js
+++ src/components/UI/UpBtn/UpBtn.js
@@ -4,7 +4,7 @@ import classes from "./UpBtn.module.css"
  const UpBtn = () =>{
   return(
     <div onClick={()=>{window.scroll(0, 0)}} className={classes.UpBtn}>
-      <img src={upbtn}/>
+      <img alt="scroll top" src={upbtn}/>
     </div>
   )
 }
