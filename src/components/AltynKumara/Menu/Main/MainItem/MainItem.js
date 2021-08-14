a75b65814b8936d60f3a9b58d93e8abe5f5698a5
Baitemir Asanbaev <baitemir1706@gmail.com>
2021-08-14 16:32:22 +0600

    menu responsibility

--- src/components/AltynKumara/Menu/Main/MainItem/MainItem.js
+++ src/components/AltynKumara/Menu/Main/MainItem/MainItem.js
@@ -3,12 +3,12 @@ import classes from "./MainItem.module.css";
 const MainItem = ({ title, composition, price, count, extra_info, weight, weight_ml }) => {
 
   return (<div className={classes.MainItem}>
-    <div>
+    <div className={classes.d_title}>
       <h2>{title ? title : null}</h2>
       <span>{composition ? composition : null}</span>
       <span>{extra_info ? extra_info : null}</span>
     </div>
-    <div>
+    <div className={classes.d_info}>
       <span>{price ? price + " сом" : null}</span>
       <span>{count ? typeof (count) === 'object' ? count[0] + " / " + count[1] + " шт" : count + " шт" : null}</span>
       <span>{weight ? typeof (weight) === 'object' ? weight[0] + "кг / " + weight[1] + "кг" : weight + " гр" : null}</span>
