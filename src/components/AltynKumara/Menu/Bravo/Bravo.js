4b249c6c2543a625fd46d0d50c6be81c9cd13a0f
Baitemir Asanbaev <baitemir1706@gmail.com>
2021-08-14 16:46:52 +0600

    bravo responsive

--- src/components/AltynKumara/Menu/Bravo/Bravo.js
+++ src/components/AltynKumara/Menu/Bravo/Bravo.js
@@ -7,15 +7,13 @@ const Bravo = () => {
   const output = Object.values(data).map((item) => {
     return (
       <div key={item.title_ru} className={classes.pizza_item}>
-        <div> 
+        <div className={classes.p_title}> 
           <h2>{item.title_ru}</h2>
           <span>{item.composition_ru}</span>
           <span>{item.price}</span>
         </div>
-        <div>
+        <div  className={classes.p_info}>
           <span> 30 см - {item.price_30} сом</span>
-          <br/>
-          <br/>
           <span> 40 см - {item.price_40} сом</span>
         </div>
 
